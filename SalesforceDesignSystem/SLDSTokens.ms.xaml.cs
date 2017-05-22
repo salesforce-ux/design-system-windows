c8fc586e21112c24c4e4e554f4f082094dfdb9de
Joe Andolina <jandolina@salesforce.com>
2017-05-22 14:22:04 -0700

    Added cs and new class calls

---
+++ SalesforceDesignSystem/SLDSTokens.ms.xaml.cs
@@ -0,0 +1,13 @@
+// Copyright (c) 2015-present, salesforce.com, inc. All rights reserved
+// Licensed under BSD 3-Clause - see LICENSE.txt or git.io/sfdc-license
+
+namespace SalesforceDesignSystem
+{
+    public partial class SLDSTokens
+    {
+        public SLDSTokens()
+        {
+            InitializeComponent();
+        }
+    }
+}
