080ab1c7031357b48ca4e6ca4fd95c6f30cdec6a
Justin Horst <justin.horst@salesforce.com>
2017-06-07 22:00:06 -0700

    add merged dictionary

---
+++ SalesforceDesignSystem/MergedResources.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace SalesforceDesignSystem
+{
+    public sealed partial class MergedResources
+    {
+        public MergedResources()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
